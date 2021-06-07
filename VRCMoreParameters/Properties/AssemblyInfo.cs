541b771a5609de1e53580bc21c2b3a9972029ec1
Kevin Birke <mail@kevinbirke.de>
2021-06-07 06:22:51 +0200

    Update AssemblyInfo.cs

--- VRCMoreParameters/Properties/AssemblyInfo.cs
+++ VRCMoreParameters/Properties/AssemblyInfo.cs
@@ -34,8 +34,8 @@ using VRCMoreParameters;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("0.0.0.1")]
 
-[assembly: MelonInfo(typeof(MyMod), "My Mod Name", "version", "Author Name")]
-[assembly: MelonGame("GameStudio", "GameName")]
+[assembly: MelonInfo(typeof(MyMod), "VRCMoreParameters", "0.0.0.1", "Dragon")]
+[assembly: MelonGame("VRChat", "VRChat")]
