0f90bc356ea850b55335858b441000093aff38cf
Kevin Birke <mail@kevinbirke.de>
2021-09-04 22:10:32 +0200

    Increase version

--- VRCMoreParameters/Properties/AssemblyInfo.cs
+++ VRCMoreParameters/Properties/AssemblyInfo.cs
@@ -34,8 +34,8 @@ using VRCMoreParameters;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.1.1.0")]
+[assembly: AssemblyFileVersion("0.1.1.0")]
 
-[assembly: MelonInfo(typeof(MyMod), "VRCMoreParameters", "0.1.0", "Dragon", "https://github.com/Dragon092/VRCMoreParameters")]
+[assembly: MelonInfo(typeof(MyMod), "VRCMoreParameters", "0.1.1", "Dragon", "https://github.com/Dragon092/VRCMoreParameters")]
 [assembly: MelonGame("VRChat", "VRChat")]
